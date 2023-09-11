031c9b90bedf192438790d37580cf034e23474be
Thibeau Wouters <thibeauwouters@gmail.com>
2023-09-11 13:44:49 +0200

    updated font color

--- myploystyle.py
+++ myploystyle.py
@@ -1,5 +1,9 @@
 plt.style.use('ggplot')
 params = {"text.usetex" : True,
           "font.family" : "serif",
+          "ytick.color" : "black",
+          "xtick.color" : "black",
+          "axes.labelcolor" : "black",
+          "axes.edgecolor" : "black",
           "font.serif" : ["Computer Modern Serif"]}
 plt.rcParams.update(params)
