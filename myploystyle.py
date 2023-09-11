783aa3d1de82e6cf8376c6ca5500ca97073570f1
Thibeau Wouters <thibeauwouters@gmail.com>
2023-09-11 13:16:34 +0200

    Create myploystyle.py

---
+++ myploystyle.py
@@ -0,0 +1,8 @@
+params = {"ytick.color" : "black",
+          "xtick.color" : "black",
+          "axes.labelcolor" : "black",
+          "axes.edgecolor" : "black",
+          "text.usetex" : True,
+          "font.family" : "serif",
+          "font.serif" : ["Computer Modern Serif"]}
+plt.rcParams.update(params)
