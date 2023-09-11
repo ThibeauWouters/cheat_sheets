b15494ab83e78bcc6aff0f95470f2ddb2184d44a
Thibeau Wouters <thibeauwouters@gmail.com>
2023-09-11 13:24:20 +0200

    added style

--- myploystyle.py
+++ myploystyle.py
@@ -1,8 +1,5 @@
-params = {"ytick.color" : "black",
-          "xtick.color" : "black",
-          "axes.labelcolor" : "black",
-          "axes.edgecolor" : "black",
-          "text.usetex" : True,
+plt.style.use('ggplot')
+params = {"text.usetex" : True,
           "font.family" : "serif",
           "font.serif" : ["Computer Modern Serif"]}
 plt.rcParams.update(params)
