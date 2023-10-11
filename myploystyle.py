77f8a0700ff1c128addcca4b8d45ab7b3fb7fde8
Thibeau Wouters <thibeauwouters@gmail.com>
2023-10-11 15:05:27 +0200

    Update myploystyle.py fontsizes

--- myploystyle.py
+++ myploystyle.py
@@ -6,5 +6,12 @@ params = {"axes.grid": True,
           "xtick.color" : "black",
           "axes.labelcolor" : "black",
           "axes.edgecolor" : "black",
-          "font.serif" : ["Computer Modern Serif"]}
+          "font.serif" : ["Computer Modern Serif"],
+         "xtick.labelsize": 16,
+          "ytick.labelsize": 16,
+          "axes.labelsize": 16,
+          "legend.fontsize": 16,
+          "legend.title_fontsize": 16,
+          "figure.titlesize": 16
+}}
 plt.rcParams.update(params)
