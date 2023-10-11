1ad7e8fb681a9a5f417550b321686b58a744eb22
Thibeau Wouters <thibeauwouters@gmail.com>
2023-10-11 15:07:23 +0200

    typo

--- myploystyle.py
+++ myploystyle.py
@@ -7,11 +7,11 @@ params = {"axes.grid": True,
           "axes.labelcolor" : "black",
           "axes.edgecolor" : "black",
           "font.serif" : ["Computer Modern Serif"],
-         "xtick.labelsize": 16,
+          "xtick.labelsize": 16,
           "ytick.labelsize": 16,
           "axes.labelsize": 16,
           "legend.fontsize": 16,
           "legend.title_fontsize": 16,
-          "figure.titlesize": 16
-         }
+          "figure.titlesize": 16}
+
 plt.rcParams.update(params)
