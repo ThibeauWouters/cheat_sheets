604fa6cdb1dc683fc995ee64d660932b5abc833c
Thibeau Wouters <thibeauwouters@gmail.com>
2023-11-09 10:26:33 +0100

    removed spaces myploystyle.py

--- myploystyle.py
+++ myploystyle.py
@@ -1,17 +1,16 @@
-
 params = {"axes.grid": True,
-          "text.usetex" : True,
-          "font.family" : "serif",
-          "ytick.color" : "black",
-          "xtick.color" : "black",
-          "axes.labelcolor" : "black",
-          "axes.edgecolor" : "black",
-          "font.serif" : ["Computer Modern Serif"],
-          "xtick.labelsize": 16,
-          "ytick.labelsize": 16,
-          "axes.labelsize": 16,
-          "legend.fontsize": 16,
-          "legend.title_fontsize": 16,
-          "figure.titlesize": 16}
+        "text.usetex" : True,
+        "font.family" : "serif",
+        "ytick.color" : "black",
+        "xtick.color" : "black",
+        "axes.labelcolor" : "black",
+        "axes.edgecolor" : "black",
+        "font.serif" : ["Computer Modern Serif"],
+        "xtick.labelsize": 16,
+        "ytick.labelsize": 16,
+        "axes.labelsize": 16,
+        "legend.fontsize": 16,
+        "legend.title_fontsize": 16,
+        "figure.titlesize": 16}
 
 plt.rcParams.update(params)
